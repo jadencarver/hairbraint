b3d61391956a0ccfa65ff8372337b1c25bb70a84
Jaden Carver <jaden.carver@simonandschuster.com>
2023-03-02 09:49:55 -0500

    updates ui

--- src/gui.rs
+++ src/gui.rs
@@ -5,7 +5,6 @@ use gtk::gdk::RGBA;
 use glib::{Date, DateTime};
 use core::time::Duration;
 use gtk::cairo::{FontSlant, FontWeight, Rectangle};
-use librsvg;
 
 pub fn main() {
     gtk::init();
@@ -20,8 +19,27 @@ pub fn main() {
     let check: gtk::Frame = builder.object("check").unwrap();
     let book: gtk::Frame = builder.object("book").unwrap();
     let preview: gtk::DrawingArea = builder.object("preview").unwrap();
+
     let day: gtk::DrawingArea = builder.object("day_area").unwrap();
-    let viewport: gtk::Viewport = builder.object("day_viewport").unwrap();
+    let timeline: gtk::DrawingArea = builder.object("timeline_area").unwrap();
+    let timeline_viewport: gtk::Viewport = builder.object("timeline_viewport").unwrap();
+    let providers: gtk::Box = builder.object("providers_box").unwrap();
+    let providers_viewport: gtk::Viewport = builder.object("providers_viewport").unwrap();
+
+    let scale = 1.25;
+
+    let stylists = vec![
+        ("Julissa", "#alert", 135.0),
+        ("Susan", "#available", 75.0),
+        ("Richard", "#alert", 135.0),
+        ("Charlie", "#unavailable", 45.0),
+        ("Chloe", "#unavailable", 45.0),
+        ("Juliet", "#late", 0.0),
+        ("Laurell", "#unavailable", 45.0),
+        ("Oaxana", "#unavailable", 45.0),
+        ("Mikaela", "#unavailable", 45.0),
+        ("Romeo", "#missing", 90.0)
+    ];
 
     let incoming: gtk::ListBox = builder.object("incoming").unwrap();
     //let appointments: gtk::ListStore = builder.object("appointments").expect("gui.ui to define appointments");
@@ -71,75 +89,46 @@ pub fn main() {
         Inhibit(false)
     });
 
+    let stylists_ = stylists.clone();
     day.connect_draw(move |day, cairo| {
         let width = (day.allocated_width() as f64 - 20.0) / 5.0;
-        let start = 35.0;
         let color = day.style_context().color(gtk::StateFlags::NORMAL);
         let color_em = day.style_context().color(gtk::StateFlags::LINK);
         let color_bg = RGBA::new(1.0,1.0,1.0,1.0);
         let color_title = RGBA::new(0.95,0.95,0.95,1.0);
-        let scale = 1.25;
         cairo.set_line_width(1.0);
-        let status_handle = librsvg::Loader::new().read_path("share/status.svg").unwrap();
-        let status_renderer = librsvg::CairoRenderer::new(&status_handle);
-
-        let stylists = vec![
-            ("Susan", "#available", 75.0),
-            ("Richard", "#alert", 135.0),
-            ("Charlie", "#unavailable", 45.0),
-            ("Juliet", "#late", 0.0),
-            ("Romeo", "#missing", 90.0)
-        ];
 
         for i in 0..5 {
             let f = i as f64;
+            let (name, status, offset) = stylists_[i];
+            let offset = offset * scale;
 
-            cairo.rectangle(2.5 + width * f, 2.5, width - 2.5, 28.0);
-            cairo.clip();
-            cairo.set_source_rgb(color_title.red(), color_title.green(), color_title.blue());
-            cairo.paint().unwrap();
-
-            cairo.set_source_rgb(color.red(), color.green(), color.blue());
-            cairo.rectangle(2.5 + width * f, 2.5, width - 2.5, 28.0);
-            cairo.stroke().unwrap();
-
-            cairo.select_font_face("DIN Alternate", FontSlant::Normal, FontWeight::Bold);
-            cairo.set_font_size(14.0);
-            cairo.move_to(10.0 + width * f, 21.0);
-            let (name, status, offset) = stylists[i];
-            let start = start + (offset * scale);
-            cairo.show_text(name);
-            status_renderer.render_layer(&cairo, Some(status), &Rectangle::new(2.5 + width * f + width - 30.0, 5.0, 25.0, 25.0)).expect(status);
-            cairo.reset_clip();
-
-            //----
-
-            cairo.rectangle(5.0 + width * f, start, width - 5.0, 30.0 * scale);
-            cairo.rectangle(5.0 + width * f, start + 75.0 * scale, width - 5.0, 45.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset, width - 5.0, 30.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset + 75.0 * scale, width - 5.0, 45.0 * scale);
             cairo.clip();
             cairo.set_source_rgb(color_bg.red(), color_bg.green(), color_bg.blue());
             cairo.paint().unwrap();
 
             cairo.set_source_rgb(color_em.red(), color_em.green(), color_em.blue());
-            cairo.rectangle(5.0 + width * f, start, 12.0, 30.0 * scale);
-            cairo.rectangle(5.0 + width * f, start + 75.0 * scale, 12.0, 45.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset, 12.0, 30.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset + 75.0 * scale, 12.0, 45.0 * scale);
             cairo.fill().unwrap();
 
             cairo.set_source_rgb(color.red(), color.green(), color.blue());
             cairo.select_font_face("Lucida Grande", FontSlant::Normal, FontWeight::Normal);
             cairo.set_font_size(12.0);
-            cairo.move_to(20.0 + width * f, start + 15.0);
+            cairo.move_to(20.0 + width * f, offset + 15.0);
             cairo.show_text("Single Process Color");
-            cairo.move_to(20.0 + width * f, start + 30.0);
+            cairo.move_to(20.0 + width * f, offset + 30.0);
             cairo.show_text("30 min");
-            cairo.move_to(20.0 + width * f, start + 15.0 + (75.0 * scale));
+            cairo.move_to(20.0 + width * f, offset + 15.0 + (75.0 * scale));
             cairo.show_text("Haircut");
-            cairo.move_to(20.0 + width * f, start + 30.0 + (75.0 * scale));
+            cairo.move_to(20.0 + width * f, offset + 30.0 + (75.0 * scale));
             cairo.show_text("45 min");
             cairo.reset_clip();
 
-            cairo.rectangle(5.0 + width * f, start, width - 5.0, 30.0 * scale);
-            cairo.rectangle(5.0 + width * f, start + 75.0 * scale, width - 5.0, 45.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset, width - 5.0, 30.0 * scale);
+            cairo.rectangle(5.0 + width * f, offset + 75.0 * scale, width - 5.0, 45.0 * scale);
             cairo.stroke().unwrap();
         }
 
@@ -150,6 +139,11 @@ pub fn main() {
         let scheduler = scheduler.clone();
         let book = book.clone();
         let check = check.clone();
+
+        for (name, status, offset) in stylists.iter() {
+            let button: gtk::Button = gtk::Button::with_label(&name);
+            providers.pack_start(&button, true, true, 0);
+        }
 
         calendar.connect_activate(move |cal| {
             if cal.is_expanded() {
@@ -162,6 +156,25 @@ pub fn main() {
         //    app.quit();
         //});
         main.show_all();
+    });
+
+    timeline.connect_draw(move |timeline, cairo| {
+        for i in 0..96 {
+            let mut h = i / 4;
+            if h == 0 { h = 12; }
+            if h > 12 { h -= 12; }
+            let m = i % 4;
+            cairo.move_to(5.0, 12.0 + i as f64 * 15.0 * scale);
+            cairo.set_font_size(12.0);
+            cairo.select_font_face("Menlo", FontSlant::Normal, FontWeight::Normal);
+            cairo.set_line_width(0.5);
+            cairo.show_text(&format!("{:2}:{:02}", h, &["00","15","30","45"][m]));
+            cairo.move_to(0.0, i as f64 * 15.0 * scale);
+            cairo.line_to(timeline.allocated_width() as f64, i as f64 * 15.0 * scale);
+            cairo.set_source_rgb(0.0, 0.0, 0.0);
+            cairo.stroke().unwrap();
+        };
+        Inhibit(false)
     });
 
     app.run();
